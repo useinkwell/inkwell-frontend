ea23c9e724c6d1cd975a5400a8b50d0ba8c4af8e
Sherifdeen Ishola <isholasherifdeen@gmail.com>
2023-03-23 18:58:20 +0100

    Made few changes on the nav and hero section

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,16 +1,30 @@
 import Image from "next/image";
-import inkwellLogo from "@/public/images/inkwell.png"
+import inkwellLogo from "@/public/images/inkwell.png";
 export default function Nav() {
-    return ( 
-    // Mobile Navigation
-       <nav className="flex  justify-between flex-wrap content-center text-3xl fixed w-full z-[5] bg-white">
-        <Image src="/images/inkwell.png" width={110} height={110} alt="nav bars" />
-        {/* <img  src="/images/inkwell.png"/> */}
-        <div className="bars py-[.8rem] px-[.8rem]"><i className="fa-solid fa-bars"></i></div>
-       </nav>   
-        // Desktop Navigation  
-
-     );
+  return (
+    <nav className="px-8 py-4 flex justify-between flex-wrap content-center fixed w-full z-[5] bg-white">
+      <Image
+        src="/images/inkwell.png"
+        width={110}
+        height={110}
+        alt="nav bars"
+      />
+      <div className="nav__menu flex items-center">
+        <button className="hidden sm:block bg-gradient-to-b from-[#000F55] to-[#B30048] rounded-lg text-base text-white font-semibold items-center px-8 py-2 md:py-4">
+          Sign Up
+        </button>
+        <div className="hidden md:flex items-center">
+          <button className="text-base text-[#000] font-semibold px-8">
+            Log In
+          </button>
+          <button className="text-base text-[#000] font-semibold px-8">
+            About Us
+          </button>
+        </div>
+      </div>
+      <div className="bars py-[.8rem] px-[.8rem] md:hidden text-2xl">
+        <i className="fa-solid fa-bars"></i>
+      </div>
+    </nav>
+  );
 }
-
- 
--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,68 @@
-import Head from 'next/head'
-import Link from 'next/link';
-import Image from 'next/image'
-import { Inter } from '@next/font/google';
-import styles from '@/styles/Home.module.css';
-import Nav from '@/components/Nav';
-import heroImg from '@/public/images/hero-image.png';
-import ellipse from '/public/images/ellipse.png';
-const inter = Inter({ subsets: ['latin'] })
-
+/* eslint-disable react/no-unescaped-entities */
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+import Nav from "@/components/Nav";
+import heroImg from "@/public/images/hero-image.png";
+import secondSlide from "@/public/images/secondSlide.svg";
+import ellipse from "/public/images/ellipse.png";
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Inkwell | Write with passion, share with pride on Inkwell</title>
-        <meta name="description" content="The space for writers to easily share thier work, get engagements and more clients with our world-class Advertising" />
-        <meta name="keywords" content="writing, gain engagements on writing, Advertise your writing Easily"></meta>
+        <meta
+          name="description"
+          content="The space for writers to easily share thier work, get engagements and more clients with our world-class Advertising"
+        />
+        <meta
+          name="keywords"
+          content="writing, gain engagements on writing, Advertise your writing Easily"
+        ></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="images/inkwell-icon.png" />
       </Head>
       <div className="">
         <Nav />
-        <div className="relative h-screen flex justify-center items-center bg-cover bg-center">
-          <div className="text-white text-4xl z-[3] flex flex-col gap-[1rem]">
-            <h1 className=''>Share with pride</h1>
-            <h1 className=''>Write with passion</h1>
+        <div className="relative h-screen flex items-center justify-center md:justify-start bg-cover bg-center">
+          <div className="text-white md:ml-12 text-4xl z-[3] flex flex-col gap-[1rem]">
+            <h1 className="mb-8">Share with pride</h1>
+            <h1 className="">Write with passion</h1>
           </div>
           <Image
             src={heroImg}
             alt="Inkwell background image"
-            fill
-            style={{ objectFit: "cover" }}
-
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
           />
-
-
-          <button className="relative bg-white text-2xl font-bold top-[16rem] left-[1rem] px-6 py-1 rounded button border-solid border-4">
-            <Link className="gradient-text " href="/"> Sign Up  </Link>
+          <button className="block sm:hidden z-[3] bg-white rounded-lg border-solid border-2 border-pink-800 text-base font-semibold items-center px-8 py-2 absolute bottom-5 right-5">
+            <Link className="gradient-text " href="/">
+              Sign Up
+            </Link>
           </button>
         </div>
-        {/*  */}
-        <article className="p-[4rem]">
+        <article className="relative h-fit p-20 sm:p-52 md:h-screen flex items-center bg-cover bg-center md:mt-20">
           <Image
-          src={ellipse} 
-          alt="Inkwell background image"
-          width={100}
-          height={100}
+            src={secondSlide}
+            alt="Inkwell background image"
+            layout="fill"
+            objectFit="contain"
+            objectPosition="center"
+            className="items-center"
           />
-          <p className="text-inkBlue text-3xl text-center leading-normal">
-          The premier online community for writers of all levels and genres. Whether you're a seasoned veteran or just starting out, Inkwell is the perfect place to connect with other writers, share your work, and receive feedback and support.
+          <p className="text-inkBlue text-xl z-[3] text-center font-medium leading-10 max-w-full md:max-w-lg relative md:absolute md:top-32 md:right-32">
+            The premier online community for writers of all levels and genres.
+            Whether you're a seasoned veteran or just starting out, Inkwell is
+            the perfect place to connect with other writers, share your work,
+            and receive feedback and support.
           </p>
         </article>
       </div>
     </>
-  )
+  );
 }
